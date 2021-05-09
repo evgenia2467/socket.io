6786f1c9eec73061c4a1684eb48c72446cd0df10
it-zmina <userok-zmina@urk.net>
2021-05-09 17:43:38 +0300

    Add template for testing socket.io

--- socketIoServer.js
+++ socketIoServer.js
@@ -5,12 +5,8 @@ const hostname = '127.0.0.1'
 const port = 3000
 
 app.get('/', (req, res) =>
-  res.sendFile(__dirname + '/sample.html')
+  res.sendFile(__dirname + '/socketIo.html')
 );
-
-app.get('/text/', (req, res) => {
-  res.send('Plain text\n');
-});
 
 http.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
