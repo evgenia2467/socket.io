761924bdce1547a2e7117afd1a00afc8d634396e
evgenia2467 <evgeniamagas813@gmail.com>
2021-05-22 16:03:52 +0300

    server.js

--- socketIoServer.js
+++ socketIoServer.js
@@ -1,5 +1,6 @@
 const app = require('express')()
 const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 const hostname = '127.0.0.1'
 const port = 3000
@@ -8,6 +9,12 @@ app.get('/', (req, res) =>
   res.sendFile(__dirname + '/socketIo.html')
 );
 
+io.on('connection', function (socked) {
+  console.log('A user connected');
+  socked.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
+});
 http.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
