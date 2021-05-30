3bf61d864686d2cd46179cd5b04cb317709a5849
evgenia2467 <evgeniamagas813@gmail.com>
2021-05-30 17:18:12 +0300

    add chat

--- socketIoServer.js
+++ socketIoServer.js
@@ -5,16 +5,22 @@ const io = require('socket.io')(http)
 const hostname = '127.0.0.1'
 const port = 3000
 
-app.get('/', (req, res) =>
-  res.sendFile(__dirname + '/socketIo.html')
-);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/socketIo.html');
+});
 
 io.on('connection', function (socked) {
   console.log('A user connected');
   socked.on('disconnect', function () {
     console.log('A user disconnected');
   });
+
+  socked.on('chat message', function (msg) {
+    console.log('New message: ' + msg);
+    socked.broadcast.emit('chat message', msg);
+  });
 });
-http.listen(port, () => {
+
+  http.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
